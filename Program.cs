6383fbf6018985e2cfb8f90145c86fc3c96c89c7
Anna <vanile93@mail.ru>
2022-08-22 21:36:21 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿/* Console.WriteLine("Введите два числа:");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.WriteLine("Максимальное число равно " + num1);
+
+}
+else {
+     Console.WriteLine("Максимальное число равно " + num2);
+}
+
+
+
+
+Console.WriteLine("Введите три числа:");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max){
+    
+    max = num2;
+
+}
+
+if(num3 > max){
+
+    max = num3;
+
+}
+
+Console.WriteLine(max);
+
+
+
+
+Console.WriteLine("Введите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0 ){
+    
+    Console.WriteLine("Число четное");
+
+}
+
+else Console.WriteLine("Число нечетное");
+
+
+
+
+Console.WriteLine("Введите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while(count <= num){
+
+    if(count % 2 == 0){ 
+        Console.Write(count + " ");
+    }
+
+    count++;
+
+}
+
+
+*/
